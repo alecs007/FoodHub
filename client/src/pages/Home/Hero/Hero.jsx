c0bae5641e1dd1cdef360e42511cf6264f548af2
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2025-02-23 18:42:16 +0200

    Modified

--- client/src/pages/Home/Hero/Hero.jsx
+++ client/src/pages/Home/Hero/Hero.jsx
@@ -6,6 +6,11 @@ const Hero = () => {
       <div className={styles.title}>
         DISCOVER<span>.</span>COOK<span>.</span>ENJOY<span>!</span>
       </div>
+      <input
+        className={styles.search}
+        type="text"
+        placeholder="Search for food recipes.."
+      />
     </section>
   );
 };
