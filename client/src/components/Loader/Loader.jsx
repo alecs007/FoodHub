be499cff49a93f625632bfe5031e7907d4c3a387
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2025-02-24 23:05:12 +0200

    Update Loader.jsx

--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import styles from "./Loader.module.css";
 const Loader = () => {
   return (
     <div className={styles.loader}>
-      <div className={styles.spinner}></div>
+      <div className={styles.spinner}>asd</div>
     </div>
   );
 };
