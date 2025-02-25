44b1dc40eb5848b6983a9eff79740ae02d260b15
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2025-02-25 16:34:07 +0200

    Modified

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,7 +1,8 @@
 import styles from "./Layout.module.css";
 import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Sidebar from "../components/Sidebar/Sidebar";
 import Footer from "../components/Footer/Footer";
@@ -9,6 +10,12 @@ import Loader from "../components/Loader/Loader";
 
 const Layout = ({ setCategoryTerm, setSearchTerm }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, [pathname]);
   return (
     <div className={styles.layout}>
       <Header
